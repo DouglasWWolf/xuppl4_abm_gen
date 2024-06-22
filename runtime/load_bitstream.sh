ae3e5b8c2a8c06d488ee444ad96a3b0e824c0daf
DouglasWWolf <douglaswolf@yahoo.com>
2024-06-22 01:33:28 -0700

    No commit message

--- runtime/load_bitstream.sh
+++ runtime/load_bitstream.sh
@@ -1,1 +1,3 @@
-load_bitstream -hot_reset -config load_jtag.conf -part xcvu3p_0 xuppl4_abm_gen.bit
+filename=$1
+test -z $filename && filename=xuppl4_abm_gen.bit
+load_bitstream -hot_reset -part xcvu3p_0 -pci_device 10ee:903f -vivado $VIVADO $filename
