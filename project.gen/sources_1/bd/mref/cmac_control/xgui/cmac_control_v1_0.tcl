b15ffc1c331ee33cfc8f1e126b8846b2066a1a5b
DouglasWWolf <douglaswolf@yahoo.com>
2024-07-12 23:04:37 -0700

    No commit message

--- project.gen/sources_1/bd/mref/cmac_control/xgui/cmac_control_v1_0.tcl
+++ project.gen/sources_1/bd/mref/cmac_control/xgui/cmac_control_v1_0.tcl
@@ -4,6 +4,7 @@ proc init_gui { IPINST } {
   #Adding Page
   set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
   ipgui::add_param $IPINST -name "RSFEC" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "TX_DIFF" -parent ${Page_0}
   ipgui::add_param $IPINST -name "TX_PRECURSOR" -parent ${Page_0}
 
 
@@ -15,6 +16,15 @@ proc update_PARAM_VALUE.RSFEC { PARAM_VALUE.RSFEC } {
 
 proc validate_PARAM_VALUE.RSFEC { PARAM_VALUE.RSFEC } {
 	# Procedure called to validate RSFEC
+	return true
+}
+
+proc update_PARAM_VALUE.TX_DIFF { PARAM_VALUE.TX_DIFF } {
+	# Procedure called to update TX_DIFF when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.TX_DIFF { PARAM_VALUE.TX_DIFF } {
+	# Procedure called to validate TX_DIFF
 	return true
 }
 
@@ -36,5 +46,10 @@ proc update_MODELPARAM_VALUE.RSFEC { MODELPARAM_VALUE.RSFEC PARAM_VALUE.RSFEC } 
 proc update_MODELPARAM_VALUE.TX_PRECURSOR { MODELPARAM_VALUE.TX_PRECURSOR PARAM_VALUE.TX_PRECURSOR } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
 	set_property value [get_property value ${PARAM_VALUE.TX_PRECURSOR}] ${MODELPARAM_VALUE.TX_PRECURSOR}
+}
+
+proc update_MODELPARAM_VALUE.TX_DIFF { MODELPARAM_VALUE.TX_DIFF PARAM_VALUE.TX_DIFF } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.TX_DIFF}] ${MODELPARAM_VALUE.TX_DIFF}
 }
 
